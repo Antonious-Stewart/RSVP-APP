0e81a2a4bce34db2a9207068cc61f5e630b51286
antonious-stewart <antonious.stewart@gmail.com>
2019-07-26 18:55:48 -0400

    add Provider, store and Router

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,15 @@
 import React, { Component } from 'react';
-
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import store from './store';
 export default class App extends Component {
 	render() {
-		return <div />;
+		return (
+			<Provider store={store}>
+				<Router>
+					<Route exact path='/' />
+				</Router>
+			</Provider>
+		);
 	}
 }
