8f19cd6ed5439ef1bffbabae02a2e6f41c873715
antonious-stewart <antonious.stewart@gmail.com>
2019-07-30 10:06:54 -0400

    import modules and init state

--- client/src/store/reducers/Auth/authReducer.test.js
+++ client/src/store/reducers/Auth/authReducer.test.js
@@ -1,8 +1,23 @@
 import authReducer from './authReducer';
+import * as actionTypes from '../../actions/Auth/types';
+import * as actionCreators from '../../actions/Auth/creators';
+import { store } from '../../../Utils/testUtils';
+const setupReducer = (
+	state = {
+		isAuth: null,
+		loading: true,
+		token: '',
+		user: null
+	},
+	action = {}
+) => {
+	return authReducer(state, action);
+};
 
 describe('Auth reducer', () => {
+	let reducer;
 	test('get default auth state', () => {
-		const reducer = authReducer(undefined, {});
+		reducer = setupReducer(undefined, {});
 		expect(reducer).toEqual({
 			isAuth: null,
 			loading: true,
