3e94a01f59fa41445b9f6482e0789a2a44a020c7
antonious-stewart <antonious.stewart@gmail.com>
2019-08-21 13:44:35 -0400

    debugging

--- db/mongoose.js
+++ db/mongoose.js
@@ -6,5 +6,6 @@ const mongoose = require('mongoose');
 mongoose.connect(process.env.MONGODB, {
 	useCreateIndex: true,
 	useFindAndModify: false,
-	useNewUrlParser: true
+	useNewUrlParser: true,
+	useMongoClient: true
 });
