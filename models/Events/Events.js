d80f7878219dc5f9bee83b8d64363b4481d78d41
antonious-stewart <antonious.stewart@gmail.com>
2019-08-24 10:26:39 -0400

    add date validation when creating events

--- models/Events/Events.js
+++ models/Events/Events.js
@@ -42,12 +42,19 @@ const EventsSchema = new mongoose.Schema({
 	rsvps: [String],
 	created: {
 		type: Date,
-		default: new Date().toDateString(),
+		default: new Date.now(),
 		required: true
 	},
 	date: {
 		type: Date,
-		required: true
+		required: true,
+		validate(value) {
+			if (
+				!Object.is(new Date(value).getFullYear() > new Date().getFullYear())
+			) {
+				throw new Error('Must Be within this year');
+			}
+		}
 	}
 });
 EventsSchema.pre('remove', async function(next) {
